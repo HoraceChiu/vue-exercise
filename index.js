6fd4897320c8d7601d24c5ba05a42869a226eac0
赵金灏 <57647858+HoraceChiu@users.noreply.github.com>
2020-04-29 07:10:08 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import App from "./myApp";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+// App 是我们的最顶级页面
+// 将App.vue 当成组件， 写了新的页面模板 myApp.vue, 拼接了原来的
+// 模块
+new Vue({ el: "#app", render: h => h(App) });
---
+++ myApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <App></App>
+    <App></App>
+  </div>
+</template>
+
+<script>
+import App from "./App";
+export default {
+  data() {
+    return {
+      name: "myApp",
+      show: true
+    };
+  },
+  components: {
+    App: App
+  }
+};
+</script>
